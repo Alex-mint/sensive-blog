8e4417f926694f66536f3d3b547221b190074b24
Alex <soyvitaly@gmail.com>
2021-11-04 11:49:20 +0100

    reduce db queries number

--- blog/views.py
+++ blog/views.py
@@ -12,7 +12,11 @@ def serialize_post(post):
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
-        'tags': [serialize_tag(tag) for tag in post.tags.all()],
+        'tags': Tag.objects.filter(
+                id__in=[tag.id for tag in post.tags.all()]
+            ).annotate(
+                posts_with_tag=Count('posts')
+            ).values('title', 'posts_with_tag'),
         'first_tag_title': post.tags.all()[0].title,
     }
 
@@ -39,11 +43,11 @@ def serialize_tag(tag):
 
 def index(request):
     most_popular_posts = Post.objects.popular()[:5] \
-        .prefetch_related('author') \
+        .prefetch_related('author', 'tags') \
         .fetch_with_comments_count()
 
     most_fresh_posts = Post.objects.fresh()[:5] \
-        .prefetch_related('author') \
+        .prefetch_related('author', 'tags') \
         .fetch_with_comments_count()
 
     most_popular_tags = Tag.objects.popular()[:5]
@@ -88,7 +92,7 @@ def post_detail(request, slug):
     most_popular_tags = Tag.objects.popular()[:5]
 
     most_popular_posts = Post.objects.popular() \
-        .prefetch_related('author')[:5] \
+        .prefetch_related('author', 'tags')[:5] \
         .fetch_with_comments_count()
 
     context = {
@@ -105,12 +109,12 @@ def tag_filter(request, tag_title):
     tag = Tag.objects.get(title=tag_title)
 
     most_popular_posts = Post.objects.popular()[:5] \
-        .prefetch_related('author') \
+        .prefetch_related('author', 'tags') \
         .fetch_with_comments_count()
 
     most_popular_tags = Tag.objects.popular()[:5]
     related_posts = tag.posts.all()[:20] \
-        .prefetch_related('author') \
+        .prefetch_related('author', 'tags') \
         .fetch_with_comments_count()
 
     context = {
